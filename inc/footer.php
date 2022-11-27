f86759d5d9c528be27f3459851257c08ce9e31b0
CharlyWells <92895394+CharlyWells@users.noreply.github.com>
2022-11-27 13:11:42 -0800

    commit

---
+++ inc/footer.php
@@ -0,0 +1,60 @@
+  <footer class="footer-2">
+    <div class="container">
+      <div class="row"> 
+        
+        <!-- Support -->
+        <div class="col-sm-7">
+          <h6>About Us</h6>
+          <div class="about-foot">
+            <p> Care and Cure Specialized Hospital, a concern of Care and Cure Group is the only multi-disciplinary super-specialty tertiary care hospital in Bangladesh, confidently providing comprehensive health care with the latest medical, surgical and diagnostic facilities. These services are provided by expert medical professionals, skilled nurses and technologists using state-of-the-art technology. </p>
+          </div>
+        </div>
+        
+       
+        
+        <!-- Conatct -->
+        <div class="col-sm-5">
+          <div class="con-info">
+            <h6>Contact Us</h6>
+            <ul>
+              <li class="mobile">
+                <p>0272312737</p>
+              </li>
+              <li class="email">
+                <p>wellingtoncharlottenaaodarley@gmail.com</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
+  <!-- Rights -->
+  <div class="rights style-2">
+    <div class="container">
+      <p>© 2022 DE PROJECT MATS. Made by DE PROJECT MATS GROUP 1</p>
+    </div>
+  </div>
+  
+  <!-- GO TO TOP  --> 
+  <a href="#" class="cd-top"><i class="fa fa-angle-up"></i></a> 
+  <!-- GO TO TOP  --> 
+</div>
+<!-- End Page Wrapper --> 
+
+<!-- JavaScripts --> 
+<script src="js/vendors/jquery/jquery.min.js"></script> 
+<script src="js/vendors/wow.min.js"></script> 
+<script src="js/vendors/bootstrap.min.js"></script> 
+<script src="js/vendors/own-menu.js"></script> 
+<script src="js/vendors/jquery.sticky.js"></script> 
+<script src="js/vendors/owl.carousel.min.js"></script> 
+<script src="js/vendors/jquery.magnific-popup.min.js"></script> 
+
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  --> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.t.min.js"></script> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.min.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>

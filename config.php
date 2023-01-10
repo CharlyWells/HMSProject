cc091df5a8705fe2944c591000bfd516b512d5b8
CharlyWells <92895394+CharlyWells@users.noreply.github.com>
2023-01-09 19:01:37 -0800

    commit

--- config.php
+++ config.php
@@ -2,4 +2,9 @@
 
 $conn = mysqli_connect('localhost','root','','user_db');
 
+$db_host = "eu-cdbr-west-03.cleardb.net ";
+$db_user = "be79d0c87fa320";
+$db_pass = "fd13a1f3 ";
+$db_name = "heroku_14f0dcd2992fec2";
+
 ?>
